78c3af8ea9a02b50b71b5389a0d30c76a6b08474
alinjar1996 <alinjardannitdgp2014@gmail.com>
2025-06-10 11:45:10 +0300

    Cost updated. Still going to Nan during trajectory. issue #4

--- sampling_based_planner/run_mpc_planner.py
+++ sampling_based_planner/run_mpc_planner.py
@@ -14,7 +14,7 @@ results = run_cem_planner(
     
     maxiter_cem=1,      # CEM iterations: Use More iterations for better convergence     
     maxiter_projection=20,   # Projection Filter iterations: Use More iterations for better Filtering
-    w_pos=40.0,      # weight on position error
+    w_pos=20.0,      # weight on position error
     w_rot=3.0,       # weight on rotation error
     w_col=80.0,      # weight on collision avoidance
     
